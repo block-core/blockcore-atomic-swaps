e10b1c2be1004f01c4f98c5ba8865561501f2984
dangershony <dan.gershony@gmail.com>
2022-10-10 21:00:21 +0100

    Add code to generate addresses

---
+++ src/Blockcore.AtomicSwaps/Shared/Networks/Networks.cs
@@ -0,0 +1,32 @@
+﻿using Blockcore.Networks.Bitcoin;
+using Blockcore.Networks.Strax;
+
+namespace Blockcore.Networks
+{
+    public static class Networks1
+    {
+        public static NetworksSelector Bitcoin
+        {
+            get
+            {
+                return new NetworksSelector(() => new BitcoinMain(), () => null, () => null);
+            }
+        }
+
+        public static NetworksSelector Strax
+        {
+            get
+            {
+                return new NetworksSelector(() => new StraxMain(), () => null, () => null);
+            }
+        }
+
+        public static NetworksSelector City
+        {
+            get
+            {
+                return new NetworksSelector(() => new City.Networks.CityMain(), () => null, () => null);
+            }
+        }
+    }
+}
