824061b7d7866a50709c6a4cf2c54ec76b18d139
dangershony <dan.gershony@gmail.com>
2022-10-23 21:46:19 +0100

    improve the fetch

--- src/Blockcore.AtomicSwaps/Shared/Models.cs
+++ src/Blockcore.AtomicSwaps/Shared/Models.cs
@@ -29,6 +29,23 @@ public class CreateSwapSession
     public long AmountToBuy { get; set; }
 }
 
+public class AddressBalance
+{
+    public string address { get; set; }
+    public long balance { get; set; }
+    public long totalReceived { get; set; }
+    public long totalStake { get; set; }
+    public long totalMine { get; set; }
+    public long totalSent { get; set; }
+    public int totalReceivedCount { get; set; }
+    public int totalSentCount { get; set; }
+    public int totalStakeCount { get; set; }
+    public int totalMineCount { get; set; }
+    public long pendingSent { get; set; }
+    public long pendingReceived { get; set; }
+}
+
+
 public class Outpoint
 {
     public string transactionId { get; set; }
