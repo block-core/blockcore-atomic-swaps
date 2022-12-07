112a2739489ff070afec4ee0321d92d0aaa70b4f
dangershony <dan.gershony@gmail.com>
2022-12-07 17:37:58 +0000

    clean program.cs

--- src/Blockcore.AtomicSwaps/Server/Program.cs
+++ src/Blockcore.AtomicSwaps/Server/Program.cs
@@ -1,8 +1,5 @@
 using Blockcore.AtomicSwaps.Server;
 using Blockcore.AtomicSwaps.Server.Services;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,11 +26,7 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-   // app.UseHsts();
 }
-
-//app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
