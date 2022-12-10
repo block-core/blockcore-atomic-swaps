555ddd654f9272a7c1a30f8dac3353dc228ef825
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2022-12-10 17:18:03 +0330

    Add dark theme (#21)

---
+++ src/Blockcore.AtomicSwaps/Client/Services/LayoutService.cs
@@ -0,0 +1,59 @@
+﻿using Blockcore.AtomicSwaps.Client.Services.UserPreferences;
+using MudBlazor;
+
+namespace Blockcore.AtomicSwaps.Client.Services;
+
+public class LayoutService
+{
+	private readonly IUserPreferencesService _userPreferencesService;
+	private UserPreferences.UserPreferences _userPreferences;
+
+	public bool IsDarkMode { get; private set; } = false;
+
+	public MudTheme CurrentTheme { get; private set; }
+
+
+	public LayoutService(IUserPreferencesService userPreferencesService)
+	{
+		_userPreferencesService = userPreferencesService;
+	}
+
+	public void SetDarkMode(bool value)
+	{
+		IsDarkMode = value;
+	}
+
+	public async Task ApplyUserPreferences(bool isDarkModeDefaultTheme)
+	{
+		_userPreferences = await _userPreferencesService.LoadUserPreferences();
+		if (_userPreferences != null)
+		{
+			IsDarkMode = _userPreferences.DarkTheme;
+		}
+		else
+		{
+			IsDarkMode = isDarkModeDefaultTheme;
+			_userPreferences = new UserPreferences.UserPreferences { DarkTheme = IsDarkMode };
+			await _userPreferencesService.SaveUserPreferences(_userPreferences);
+		}
+	}
+
+	public event EventHandler MajorUpdateOccured;
+
+	private void OnMajorUpdateOccured() => MajorUpdateOccured?.Invoke(this, EventArgs.Empty);
+
+	public async Task ToggleDarkMode()
+	{
+		IsDarkMode = !IsDarkMode;
+		_userPreferences.DarkTheme = IsDarkMode;
+		await _userPreferencesService.SaveUserPreferences(_userPreferences);
+		OnMajorUpdateOccured();
+	}
+
+	public void SetBaseTheme(MudTheme theme)
+	{
+		CurrentTheme = theme;
+		OnMajorUpdateOccured();
+	}
+
+}
---
+++ src/Blockcore.AtomicSwaps/Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Blockcore.AtomicSwaps.Client.Services.UserPreferences;
+using Blockcore.AtomicSwaps.Client.Services;
+using MudBlazor;
+
+namespace Blockcore.AtomicSwaps.Client.Shared
+{
+	public partial class MainLayout : LayoutComponentBase, IDisposable
+	{
+		[Inject] private LayoutService LayoutService { get; set; }
+
+		private MudThemeProvider _mudThemeProvider;
+
+		protected override void OnInitialized()
+		{
+			LayoutService.MajorUpdateOccured += LayoutServiceOnMajorUpdateOccured;
+			LayoutService.SetBaseTheme(Theme.SwapsTheme());
+			base.OnInitialized();
+		}
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			await base.OnAfterRenderAsync(firstRender);
+
+			if (firstRender)
+			{
+				await ApplyUserPreferences();
+				StateHasChanged();
+			}
+		}
+
+		private async Task ApplyUserPreferences()
+		{
+			var defaultDarkMode = await _mudThemeProvider.GetSystemPreference();
+			await LayoutService.ApplyUserPreferences(defaultDarkMode);
+		}
+
+		public void Dispose()
+		{
+			LayoutService.MajorUpdateOccured -= LayoutServiceOnMajorUpdateOccured;
+		}
+
+		private void LayoutServiceOnMajorUpdateOccured(object sender, EventArgs e) => StateHasChanged();
+
+		bool _drawerOpen = true;
+
+		void DrawerToggle()
+		{
+			_drawerOpen = !_drawerOpen;
+		}
+	}
+}
