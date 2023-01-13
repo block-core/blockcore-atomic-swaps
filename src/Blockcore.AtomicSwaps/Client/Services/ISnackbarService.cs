92f88766d896f7ff254648e0362da495abf76ad1
Milad <miladsoft@yahoo.com>
2023-01-14 00:01:02 +0330

    Add severity to showMessage methode

--- src/Blockcore.AtomicSwaps/Client/Services/ISnackbarService.cs
+++ src/Blockcore.AtomicSwaps/Client/Services/ISnackbarService.cs
@@ -1,15 +1,10 @@
-﻿using System.ComponentModel;
+﻿using MudBlazor;
+using System.ComponentModel;
 
 namespace Blockcore.AtomicSwaps.Client.Services
 {
-    public interface ISnackbarService
-    {
-
-        Task ShowNormalMessage(string message, string position);
-        Task ShowInfoMessage(string message, string position);
-        Task ShowSuccessMessage(string message, string position);
-        Task ShowWarningMessage(string message, string position);
-        Task ShowErrorMessage(string message, string position);
-
-    }
+	public interface ISnackbarService
+	{
+		Task ShowMessage(string message, string position, Severity severity);
+	}
 }
--- src/Blockcore.AtomicSwaps/Client/Services/SnackbarService.cs
+++ src/Blockcore.AtomicSwaps/Client/Services/SnackbarService.cs
@@ -3,82 +3,27 @@ using MudBlazor;
 
 namespace Blockcore.AtomicSwaps.Client.Services
 {
-    public class SnackbarService : ISnackbarService
-    {
-        private readonly ISnackbar _snackbar;
+	public class SnackbarService : ISnackbarService
+	{
+		private readonly ISnackbar _snackbar;
 
-        public SnackbarService(ISnackbar snackbar)
-        {
-            _snackbar = snackbar;
-        }
+		public SnackbarService(ISnackbar snackbar)
+		{
+			_snackbar = snackbar;
+		}
 
-        /// <summary>
-        /// position =  Defaults.Classes.Position.X
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public async Task ShowErrorMessage(string message, string position)
-        {
-            _snackbar.Clear();
-            _snackbar.Configuration.PositionClass = position;
-            _snackbar.Add(message, Severity.Error);
-        }
+		/// <summary>
+		/// position =  Defaults.Classes.Position.X
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="position"></param>
+		/// <returns></returns>
+		public async Task ShowMessage(string message, string position, Severity severity)
+		{
+			_snackbar.Clear();
+			_snackbar.Configuration.PositionClass = position;
+			_snackbar.Add(message, severity);
+		}
 
-
-        /// <summary>
-        /// position =  Defaults.Classes.Position.X
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public async Task ShowInfoMessage(string message, string position)
-        {
-            _snackbar.Clear();
-            _snackbar.Configuration.PositionClass = position;
-            _snackbar.Add(message, Severity.Info);
-        }
-
-
-        /// <summary>
-        /// position =  Defaults.Classes.Position.X
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public async Task ShowNormalMessage(string message, string position)
-        {
-            _snackbar.Clear();
-            _snackbar.Configuration.PositionClass = position;
-            _snackbar.Add(message, Severity.Normal);
-        }
-
-
-        /// <summary>
-        /// position =  Defaults.Classes.Position.X
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public async Task ShowSuccessMessage(string message, string position)
-        {
-            _snackbar.Clear();
-            _snackbar.Configuration.PositionClass = position;
-            _snackbar.Add(message, Severity.Success);
-        }
-
-
-        /// <summary>
-        /// position =  Defaults.Classes.Position.X
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public async Task ShowWarningMessage(string message, string position)
-        {
-            _snackbar.Clear();
-            _snackbar.Configuration.PositionClass = position;
-            _snackbar.Add(message, Severity.Warning);
-        }
-    }
+	}
 }
