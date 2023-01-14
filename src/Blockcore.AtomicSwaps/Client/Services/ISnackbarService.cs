34d1bedc1afa97631870de95b408eb86511c298d
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2023-01-15 00:21:03 +0330

    Atomic swap UI (#31)
    * Add SnackbarService
    
    * Add severity to showMessage methode
    
    * Add animation to text
    
    * UI improvements
    
    * Fixed a few small bugs
    
    * Update src/Blockcore.AtomicSwaps/Client/Pages/CreateSwap.razor
    
    Co-authored-by: Dan Gershony <dan.gershony@gmail.com>
    
    Co-authored-by: Dan Gershony <dan.gershony@gmail.com>

---
+++ src/Blockcore.AtomicSwaps/Client/Services/ISnackbarService.cs
@@ -0,0 +1,10 @@
+﻿using MudBlazor;
+using System.ComponentModel;
+
+namespace Blockcore.AtomicSwaps.Client.Services
+{
+	public interface ISnackbarService
+	{
+		Task ShowMessage(string message, string position, Severity severity);
+	}
+}
---
+++ src/Blockcore.AtomicSwaps/Client/Services/SnackbarService.cs
@@ -0,0 +1,29 @@
+﻿
+using MudBlazor;
+
+namespace Blockcore.AtomicSwaps.Client.Services
+{
+	public class SnackbarService : ISnackbarService
+	{
+		private readonly ISnackbar _snackbar;
+
+		public SnackbarService(ISnackbar snackbar)
+		{
+			_snackbar = snackbar;
+		}
+
+		/// <summary>
+		/// position =  Defaults.Classes.Position.X
+		/// </summary>
+		/// <param name="message"></param>
+		/// <param name="position"></param>
+		/// <returns></returns>
+		public async Task ShowMessage(string message, string position, Severity severity)
+		{
+			_snackbar.Clear();
+			_snackbar.Configuration.PositionClass = position;
+			_snackbar.Add(message, severity);
+		}
+
+	}
+}
