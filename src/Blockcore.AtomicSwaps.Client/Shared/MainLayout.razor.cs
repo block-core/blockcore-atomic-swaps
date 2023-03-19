4303f664fb07baacb5073562ff680f7431307bfe
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2023-03-19 23:26:08 +0400

    Add slide show

--- src/Blockcore.AtomicSwaps.Client/Shared/MainLayout.razor.cs
+++ src/Blockcore.AtomicSwaps.Client/Shared/MainLayout.razor.cs
@@ -4,51 +4,57 @@ using Microsoft.AspNetCore.Components;
 using Blockcore.AtomicSwaps.Client.Services.UserPreferences;
 using Blockcore.AtomicSwaps.Client.Services;
 using MudBlazor;
+using Blockcore.AtomicSwaps.Client.Pages.Dialogs;
 
 namespace Blockcore.AtomicSwaps.Client.Shared
 {
-	public partial class MainLayout : LayoutComponentBase, IDisposable
-	{
-		[Inject] private LayoutService LayoutService { get; set; }
+    public partial class MainLayout : LayoutComponentBase, IDisposable
+    {
+        [Inject] private LayoutService LayoutService { get; set; }
 
-		private MudThemeProvider _mudThemeProvider;
+        private MudThemeProvider _mudThemeProvider;
 
-		protected override void OnInitialized()
-		{
-			LayoutService.MajorUpdateOccured += LayoutServiceOnMajorUpdateOccured;
-			LayoutService.SetBaseTheme(Theme.Theme.SwapsTheme());
-			base.OnInitialized();
-		}
+        protected override void OnInitialized()
+        {
+            LayoutService.MajorUpdateOccured += LayoutServiceOnMajorUpdateOccured;
+            LayoutService.SetBaseTheme(Theme.Theme.SwapsTheme());
+            base.OnInitialized();
+        }
 
-		protected override async Task OnAfterRenderAsync(bool firstRender)
-		{
-			await base.OnAfterRenderAsync(firstRender);
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
 
-			if (firstRender)
-			{
-				await ApplyUserPreferences();
-				StateHasChanged();
-			}
-		}
+            if (firstRender)
+            {
+                await ApplyUserPreferences();
+                StateHasChanged();
+            }
+        }
 
-		private async Task ApplyUserPreferences()
-		{
-			var defaultDarkMode = await _mudThemeProvider.GetSystemPreference();
-			await LayoutService.ApplyUserPreferences(defaultDarkMode);
-		}
+        private async Task ApplyUserPreferences()
+        {
+            var defaultDarkMode = await _mudThemeProvider.GetSystemPreference();
+            await LayoutService.ApplyUserPreferences(defaultDarkMode);
+        }
 
-		public void Dispose()
-		{
-			LayoutService.MajorUpdateOccured -= LayoutServiceOnMajorUpdateOccured;
-		}
+        public void Dispose()
+        {
+            LayoutService.MajorUpdateOccured -= LayoutServiceOnMajorUpdateOccured;
+        }
 
-		private void LayoutServiceOnMajorUpdateOccured(object sender, EventArgs e) => StateHasChanged();
+        private void LayoutServiceOnMajorUpdateOccured(object sender, EventArgs e) => StateHasChanged();
 
-		bool _drawerOpen = true;
+        bool _drawerOpen = true;
 
-		void DrawerToggle()
-		{
-			_drawerOpen = !_drawerOpen;
-		}
-	}
+        void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+        private void OpenDialog()
+        {
+            var options = new DialogOptions { CloseOnEscapeKey = true };
+            DialogService.Show<Github>("Github", options);
+        }
+    }
 }
