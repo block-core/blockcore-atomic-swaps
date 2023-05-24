2fa73d95a046d3001126237d88fbb37e74a10497
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2023-05-24 19:58:46 +0400

    Create private swaps (#57)
    * Create private swaps
    
    * Added a new method to post pubkeys to the server side
    
    * Update FindSwaps.razor

--- src/Blockcore.AtomicSwaps.Server/Controllers/SwapCoordinatorController.cs
+++ src/Blockcore.AtomicSwaps.Server/Controllers/SwapCoordinatorController.cs
@@ -15,21 +15,25 @@ namespace Blockcore.AtomicSwaps.Server.Controllers
 
         public SwapCoordinatorController(ILogger<SwapCoordinatorController> logger, IStorageService storageService)
         {
-	        _logger = logger;
-	        _storageService = storageService;
+            _logger = logger;
+            _storageService = storageService;
         }
 
         [HttpGet]
         public async Task<IEnumerable<SwapSession>> Get()
         {
-	        return await _storageService.Get();
+            return await _storageService.Get();
         }
-
+        [HttpPost]
+        public async Task<IEnumerable<SwapSession>> Post(List<string> pubKeys)
+        {
+            return await _storageService.Post(pubKeys);
+        }
         [HttpGet]
         [Route("session/{swapSessionId}")]
         public async Task<SwapSession?> GetBySession(string swapSessionId)
         {
-	        return await _storageService.Get(swapSessionId);
+            return await _storageService.Get(swapSessionId);
         }
 
         [HttpPost]
@@ -38,38 +42,38 @@ namespace Blockcore.AtomicSwaps.Server.Controllers
         {
             await _storageService.Add(data);
         }
-        
+
         [HttpPost]
         [Route("update")]
         public async Task<IActionResult> UpdateSession(SwapSession data)
         {
-	        var swapSession = await this.GetBySession(data.SwapSessionId);
+            var swapSession = await this.GetBySession(data.SwapSessionId);
 
-	        if (swapSession == null)
-	        {
-		        return NotFound();
-	        }
+            if (swapSession == null)
+            {
+                return NotFound();
+            }
 
-	        if (data.SwapMaker.SenderPubkey != swapSession.SwapMaker.SenderPubkey)
-		        throw new Exception("Invalid SwapMaker owner");
+            if (data.SwapMaker.SenderPubkey != swapSession.SwapMaker.SenderPubkey)
+                throw new Exception("Invalid SwapMaker owner");
 
-	        if (swapSession.SwapTaker.SenderPubkey != null && data.SwapTaker.SenderPubkey != swapSession.SwapTaker.SenderPubkey)
-		        throw new Exception("Invalid SwapTaker owner");
+            if (swapSession.SwapTaker.SenderPubkey != null && data.SwapTaker.SenderPubkey != swapSession.SwapTaker.SenderPubkey)
+                throw new Exception("Invalid SwapTaker owner");
 
-	        await _storageService.Update(data);
+            await _storageService.Update(data);
 
-	        return Ok();
+            return Ok();
         }
 
         [HttpDelete]
         [Route("delete/{swapSessionId}")]
         public async Task DeleteSession(string swapSessionId)
         {
-	        var swap = await _storageService.Get(swapSessionId);
-	        if (swap != null)
-	        {
-		        await _storageService.Complete(swap);
-	        }
+            var swap = await _storageService.Get(swapSessionId);
+            if (swap != null)
+            {
+                await _storageService.Complete(swap);
+            }
         }
-	}
+    }
 }
