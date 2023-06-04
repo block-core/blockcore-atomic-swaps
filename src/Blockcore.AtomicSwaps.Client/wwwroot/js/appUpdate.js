2dc0e3c7932409c20bc7c5ededef0c2e4355b509
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2023-06-04 12:26:38 +0400

    Add pop up to install app and update to new version

---
+++ src/Blockcore.AtomicSwaps.Client/wwwroot/js/appUpdate.js
@@ -0,0 +1,34 @@
+﻿var atomicSwaps = window.AtomicSwaps || {};
+atomicSwaps.mainBodyInstance = null;
+
+const bc = new BroadcastChannel('blockcore-atomicswaps-channel');
+bc.onmessage = function (message) {
+    if (message && message.data == "new-version-found") {
+        notifyAppUpdateToUser();
+    } else if (message && message.data == "reload-page") {
+        setTimeout(function () {
+            //location.reload();
+            window.location.href = window.location.href;
+        }, 500);
+    }
+}
+
+function notifyAppUpdateToUser() {
+    setTimeout(function () {
+        if (atomicSwaps.mainBodyInstance) {
+            atomicSwaps.mainBodyInstance.invokeMethodAsync('ShowUpdateVersion').then(function () { }, function (er) {
+                //setTimeout(notifyAppUpdateToUser, 2000);
+            });
+        }
+    }, 2000);
+}
+atomicSwaps.onUserUpdate = function () {
+    setTimeout(function () {
+        bc.postMessage("skip-waiting");
+    }, 300);
+}
+
+var mainBodyInstance;
+function setRef(ref) {
+    atomicSwaps.mainBodyInstance = ref;
+}
